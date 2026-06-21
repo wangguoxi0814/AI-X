bff766a5af0bc6e4ebe4dd77dd19a7fb2ff7e3ca
wangguoxi <wangguoxi@darryring.com>
2026-06-21 16:40:46 +0800

    fix(rag):解决因确实@RestController导致接口请求404问题

--- ai-x-api/src/main/java/com/aix/api/controller/rag/RagController.java
+++ ai-x-api/src/main/java/com/aix/api/controller/rag/RagController.java
@@ -8,9 +8,11 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 @RequestMapping("/rag")
 @Validated
 public class RagController {
